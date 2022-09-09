1ef029029051e1b5b39c493f692faa3b1c3205b7
hannalampinen <n0laha00@students.oamk.fi>
2022-09-09 16:04:09 +0300

    Valmis alert tehtävä

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default function App() {
+
+  // Kun napista painetaan (onPress), showAlert-funktio aktivoituu
+  // showAlert luo in-built Alert-funktiolla viestin jossa otsikkona my title ja tekstinä ...
+  // Text-kohdassa olevat tekstikentät ovat vaihtoehdot mitä 'vastata viestiin' esim accept/cancel tms.
+
+  const [ok, setOk] = useState(false);
+
+  const showAlert = () => {
+    Alert.alert(
+      'Hello...',
+      'Are you lost bby gurl?',
+    [ 
+      {
+        text: 'Ok',
+        onPress: () => setOk(true)
+        // kun käyttäjä painaa ok, setOk-funktio asettaa ok-muuttujan arvoksi true
+        // näytöllä lukee ok
+      },
+      {
+        text: 'Cancel',
+        onPress: () => setOk(false)
+        // kun käyttäjä painaa cancel, setOk-funktio asettaa ok-muuttujan arvoksi false
+        // näytöllä lukee false
+      }
+    ]
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Press' onPress={showAlert}></Button>
+      <Text>{ok===true ? 'ok' : 'cancel'}</Text>
+      {/* ok-muuttuja arvo on true ? näytöllä lukee silloin ok : jos false, näytöllä lukee cancel */}
     </View>
   );
 }
